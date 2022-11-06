e125d1921e11c4c590eb8bc081f29941008178b4
Adit-ahnaf <aditahnaf73@gmail.com>
2022-11-06 14:43:14 +0600

    code updated

--- addBinary.js
+++ addBinary.js
@@ -1,27 +1,27 @@
-let a  = '11'
-let b = '1'
+let a  = '1010'
+let b = '1011'
 
 function fnd (a, b){
-let i = a.length - 1;
-let j = b.length - 1;
+let i = a.length - 1; //0
+let j = b.length - 1;//1
 let sum;
 let carry = 0;
 let res = '';
 while(i >= 0 || j >= 0){
- sum = carry
- if( i >= 0 ) sum += a[i] - '0'
- if( j >= 0 ) sum += b[j] - '0'
- 
- res  = res + (sum % 2)
+ sum = carry //0
+ if( i >= 0 ) sum += a[i] - '0' // 0
+ if( j >= 0 ) sum += b[j] - '0'//1
+ //s - 1
+ res  = res + (sum % 2)//0 + 1 --- 1
  res.toString()
- carry = sum / 2
+ carry = Math.floor(sum / 2)
  i--, j--
 
 }
 
 if(carry != 0 ) res += '1';
 
-res = res.split("").reverse().join("");
+// res = res.split("").reverse().join("");
 
 return res
 }
